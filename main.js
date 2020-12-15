be24ab91d2e30e5e6d6c0d911de3f7a77dda16bb
codingwolf-at <atameshwari9@gmail.com>
2020-12-15 22:41:40 +0530

    feat: taking input

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const element = document.querySelector('#btn-counter');
+const element = document.querySelector('#btnAddExpense');
+const inputText = document.querySelector('#inputAmount');
 
-let counter = 0;
+let totalExpense = 0;
 
-function likeCountHandler() {
-    counter++;
-    console.log(counter);
+function addExpense() {
+    const inputData = inputText.value;
+    const expense = parseInt(inputData, 10);
+    totalExpense = totalExpense + expense;
 }
 
-element.addEventListener("click", likeCountHandler);
+element.addEventListener("click", addExpense, false);
