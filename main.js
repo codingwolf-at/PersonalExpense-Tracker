6ff31dbcaccbe50630325e1c0c00c0f163b81e99
codingwolf-at <atameshwari9@gmail.com>
2020-12-16 14:12:27 +0530

    feat: array of objects for expense tracking

--- main.js
+++ main.js
@@ -10,22 +10,37 @@ const element = document.querySelector('#btnAddExpense');
 // init value of total expense to 0
 let totalExpense = 0;
 
+// init an array for all expenses
+const allExpense = [];
+
 // init value of heading element to total expense 
 headingElement.textContent = totalExpense;
 
 // onButtonCLick add expense amount to total expense 
 function addExpense() {
+    // create an object for storing amount & decs pair
+    const expenseItem = {};
+
     // reading amount input element value
-    const inputAmountData = inputAmount.value;
+    const expense = inputAmount.value;
     
     // reading description input element  
     const inputDescData = inputDesc.value;
 
     // converting expense from string to number
-    const expense = parseInt(inputAmountData, 10);
+    const inputAmountData = parseInt(expense, 10);
+
+    // init value of amount in object 
+    expenseItem.amount = inputAmountData;
+
+    // init value of desc in object
+    expenseItem.desc = inputDescData
+
+    // pushing object to array
+    allExpense.push(expenseItem);
     
     // adding expense to total expense
-    totalExpense = totalExpense + expense;
+    totalExpense = totalExpense + inputAmountData;
 
     // set the value of heading element to total expense 
     headingElement.textContent = totalExpense;
