802fb72d36985cb24c379037f0ae7fdbe42c0de4
codingwolf-at <atameshwari9@gmail.com>
2020-12-16 13:50:39 +0530

    feat: input description element

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+// ref to h1 element
+const headingElement = document.querySelector('#heading')
+// ref to input amount 
+const inputAmount = document.querySelector('#inputAmount');
+// ref to input description
+const inputDesc = document.querySelector('#inputDesc')
+// ref to add expense button
 const element = document.querySelector('#btnAddExpense');
-const inputText = document.querySelector('#inputAmount');
 
+// init value of total expense to 0
 let totalExpense = 0;
 
+// init value of heading element to total expense 
+headingElement.textContent = totalExpense;
+
+// onButtonCLick add expense amount to total expense 
 function addExpense() {
-    const inputData = inputText.value;
-    const expense = parseInt(inputData, 10);
+    // reading amount input element value
+    const inputAmountData = inputAmount.value;
+    
+    // reading description input element  
+    const inputDescData = inputDesc.value;
+
+    // converting expense from string to number
+    const expense = parseInt(inputAmountData, 10);
+    
+    // adding expense to total expense
     totalExpense = totalExpense + expense;
+
+    // set the value of heading element to total expense 
+    headingElement.textContent = totalExpense;
 }
 
+// adding event listener to add expense button
 element.addEventListener("click", addExpense, false);
